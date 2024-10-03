96d811ab5d39edc379bc3201366e86d72ba8f31d
zilah7 <163401104+zilah7@users.noreply.github.com>
2024-10-03 17:33:32 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+//peerbedaan function biasa dan arrow function
+
+//function biasa
+function penjumlahan (a,f){
+    return jumlah = a + f;
+}
+console.log(penjumlahan(1,5));
+
+//arrow function
+const input = (y,i) => y + i;
+console.log(input(1,5));
+
+//kesimpulannya
+//function biasa menggunakan kata kunci  function memiliki sintalsis panjang
+//arrow function fungsi yang sama namun lebih ringkas dan lebih banyak operator ternary
